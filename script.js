129cf3355753392f9606055ef9fa31d65f84256e
AmmarAdnan29 <ammaradnan1786@gmail.com>
2025-05-09 16:21:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,4 @@ function showOrderPopup(name, price) {
     document.getElementById('orderPopup').style.display = 'none';
   }
   
-  // Disable mobile scaling to force desktop view
-window.addEventListener('load', () => {
-  const viewport = document.querySelector("meta[name=viewport]");
-  viewport.setAttribute('content', 'width=1024'); // Set a fixed width to simulate desktop
-});
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
